24d310e376a1fc21cc6a7a7395304492ec28b68a
leogle <leogle@yeah.net>
2017-08-03 22:14:57 +0800

    BorderLayout & GridLayout

---
+++ BeginnerToMastery/src/leogle/chapter13/SwingPrograming/BorderLayoutClass.java
@@ -0,0 +1,37 @@
+package leogle.chapter13.SwingPrograming;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class BorderLayoutClass extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	// 1.定义组件摆放位置的数组
+	String[] border = { BorderLayout.CENTER, BorderLayout.NORTH, BorderLayout.SOUTH, BorderLayout.WEST,
+			BorderLayout.EAST };
+	String[] buttonName = { "center button", "north button", "south button", "west button", "east button" };
+
+	public BorderLayoutClass() {
+		setTitle("这个窗体使用边界布局管理器");
+		Container container = getContentPane();
+		// 2.设置窗体使用边界布局管理器
+		setLayout(new BorderLayout());
+		for (int i = 0; i < border.length; i++) {
+			// 3.在容器中添加按钮，并设置按钮布局
+			container.add(new JButton(buttonName[i]), border[i]);
+		}
+
+		setSize(500, 500);
+		setVisible(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new BorderLayoutClass();
+	}
+
+}
---
+++ BeginnerToMastery/src/leogle/chapter13/SwingPrograming/GridLayoutClass.java
@@ -0,0 +1,31 @@
+package leogle.chapter13.SwingPrograming;
+
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class GridLayoutClass extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public GridLayoutClass() {
+		setTitle("这个窗体使用网格布局管理器");
+		Container container = getContentPane();
+		setLayout(new GridLayout(7, 3, 5, 5));
+		for (int i = 0; i < 20; i++) {
+			container.add(new JButton("button" + (i + 1)));
+		}
+
+		setSize(500, 500);
+		setVisible(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new GridLayoutClass();
+	}
+
+}
